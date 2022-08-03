e4e3f75f3ad11ad3d5193355adbef9f2fc2086d3
Mukesh <brainstorm4x@gmail.com>
2022-08-03 13:15:17 +0530

    Added links for Show, Edit & Delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        @article = Article.find(params[:id])    
     end
 
     def create
@@ -34,5 +34,11 @@ class ArticlesController < ApplicationController
         else
             render 'edit'
         end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 end
