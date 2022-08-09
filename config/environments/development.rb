6b41212c15f1ff78a8222bb201e46d229cfbbeda
Mukesh <brainstorm4x@gmail.com>
2022-08-09 11:31:36 +0530

    Trying to deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # TURN ON INLINE SOURCE MAPS (SO WE CAN DEBUG RAILS 7 BOOTSTRAP)
+  config.sass.inline_source_maps = true
+
   # Enable server timing
   config.server_timing = true
 
